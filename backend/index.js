5e4622784c8e9dca76fe18526f32086ab5841fdb
Puneet Sharma <puneet316sharma@gmail.com>
2026-07-02 17:01:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port = process.env.PORT || 5000
 app.use(cors({
-   origin:["http://localhost:5173", "http://localhost:5174","http://localhost:5175","http://localhost:5176"],
+   origin:"https://yupp-wn71.onrender.com",
    credentials:true
 }))
    app.use(express.json())
